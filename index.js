3969b0945dc8457e025822a11b5a8f25ee761254
Brooks <brooks.gunn@vikings.berry.edu>
2018-01-05 00:28:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 var name = "Joe";
-var height = "74"
+var height = "74";
 
 
 // Don't worry about this
 module.exports = { name, height
 }
-
+if (name === susan){
+  console.log("This correct")
+}
